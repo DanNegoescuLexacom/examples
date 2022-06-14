763cfc94e5d89568845a444c9bb313a0f1884d1b
Ben Morris <ben.morris@lexacom.co.uk>
2022-06-14 11:57:00 +0100

    backup

---
+++ nullable/Program.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var nullable = new Nullable();
+MemoryStream? stream = null;
+nullable.Save(stream);
+
+public class Nullable 
+{
+    private readonly FileInfo _fileInfo;
+
+    public Nullable()
+    {}
+
+    public Nullable(FileInfo fileInfo)
+    {
+        _fileInfo = fileInfo;
+    }
+
+    public void Save(Stream inStream)
+    {
+        using (var stream = File.OpenWrite(_fileInfo.FullName))
+        {
+            inStream.CopyTo(stream);
+        }
+    }
+}
